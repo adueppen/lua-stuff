a209b9838efeff0af05957cf19326b59d98143d3
Alex Dueppen <fourdoops@gmail.com>
2017-05-10 10:19:29 -0500

    remove old stuff

--- main.lua
+++ main.lua
@@ -3,7 +3,6 @@ function love.load()
   love.window.setMode(800, 600, {fullscreen=false, resizable=false, centered=true})
   love.window.setTitle('Pong')
   love.graphics.setBackgroundColor(0, 0, 0, 255)
-  logo = love.graphics.newImage('res/pong.png')
   font = love.graphics.newImageFont('res/pongfont.png', ' abcdefghijklmnopqrstuvwxyz', 2)
   font:setFilter('nearest', 'nearest')
   love.graphics.setFont(font)
@@ -19,7 +18,6 @@ function love.draw()
   love.graphics.push()
   love.graphics.scale(5, 5)
   love.graphics.print('pong', 21, 22)
-  -- love.graphics.draw(logo, 105, 125)
   if time > .75 then
     if time > 1.5 then
       time = 0
