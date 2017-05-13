c2e6e1deffe8b22de46b97f04eaf8a56ed36bfea
Alex Dueppen <fourdoops@gmail.com>
2017-05-13 13:01:02 -0500

    More work on actual game and font changes

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ function love.load()
   love.window.setMode(800, 600, {fullscreen=false, resizable=false, centered=true})
   love.window.setTitle('Pong')
   love.keyboard.setKeyRepeat(true)
-  font = love.graphics.newImageFont('res/pongfont.png', ' abcdefghijklmnopqrstuvwxyz<>12345-', 2)
+  font = love.graphics.newImageFont('res/pongfont.png', ' abcdefghijklmnopqrstuvwxyz<>-0123456789', 2)
   font:setFilter('nearest', 'nearest')
   love.graphics.setFont(font)
   love.graphics.setLineStyle('rough')
@@ -16,8 +16,8 @@ function love.load()
   bgcolor = 1
   colorNameTable = {'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white'}
   colorTable = {{0, 0, 0}, {255, 0, 0}, {0, 255, 0}, {255, 255, 0}, {0, 0, 255}, {255, 0, 255}, {0, 255, 255}, {255, 255, 255}}
-  player = {y = 10}
-  opponent = {y = 10}
+  player = {y = 80, points = 0}
+  opponent = {y = 80, points = 0}
 end
 
 function resizeWindow()
@@ -35,7 +35,6 @@ function love.keypressed(key)
     else
       -- gamestart
       state = 'game'
-      love.window.showMessageBox('Temp message, remove later', 'This will start the game')
     end
   elseif state == 'options' then
     if key == 'escape' then
@@ -76,15 +75,16 @@ function love.keypressed(key)
       bgcolor = colorChange(bgcolor, key)
     end
   elseif state == 'game' then
-    if key == 'down' then
-      player.y = player.y + 10
-    elseif key == 'up' then
-      player.y = player.y - 10
+    if key == 'escape' then
+      state = 'title'
+      player.y = 80
+      opponent.y = 80
+      player.points = 0
+      opponent.points = 0
     end
+    -- maybe add a pause menu at some point
   end
 end
-
-
 
 function colorChange(selColor, key)
   if key == 'right' then
@@ -111,8 +111,14 @@ function goodContrast()
 end
 
 function love.update(dt)
-  if state == 'title' or state == 'options' then
+  if state == 'title' then
     time = time + dt
+  elseif state == 'game' then
+    if love.keyboard.isDown('down') then
+      player.y = player.y + 5
+    elseif love.keyboard.isDown('up') then
+      player.y = player.y - 5
+    end
   end
 end
 
@@ -121,18 +127,14 @@ function love.draw()
   love.graphics.setColor(colorTable[fgcolor])
   love.graphics.setBackgroundColor(colorTable[bgcolor])
   if state == 'title' then
-    love.graphics.push()
-    love.graphics.scale(5, 5)
-    love.graphics.print('pong', 21, 22)
+    love.graphics.print('pong', 105, 105, 0, 5, 5)
     if time > .75 then
       if time > 1.5 then
         time = 0
       end
-      love.graphics.pop()
     else
-      love.graphics.pop()
-      love.graphics.print('press the any key', 115, 185)
-      love.graphics.print('press o for options', 105, 205)
+      love.graphics.print('press the any key or', 100, 175)
+      love.graphics.print('press o for options', 105, 195)
     end
   elseif state == 'options' then
     love.graphics.print('options - press esc to exit', 5, 5)
@@ -140,7 +142,7 @@ function love.draw()
     love.graphics.print('scale < ' .. scale .. ' >', 5, 30)
     love.graphics.print('foreground color < ' .. colorNameTable[fgcolor] .. ' >', 5, 45)
     love.graphics.print('background color < ' .. colorNameTable[bgcolor] .. ' >', 5, 60)
-    -- more options
+    -- change awkward color change implementation to printf
     if selectedOption == 1 then
       goodContrast()
       love.graphics.print(' < ' .. scale .. ' >', 55, 30)
@@ -153,6 +155,10 @@ function love.draw()
       love.graphics.print(' < ' .. colorNameTable[bgcolor] .. ' >', 166, 60)
     end
   elseif state == 'game' then
+    love.graphics.rectangle('fill', 0, 60, 400, 10)
+    love.graphics.print(player.points, 100, 2, 0, 4, 4)
+    love.graphics.print(opponent.points, 260, 2, 0, 4, 4)
     love.graphics.rectangle('fill', 10, player.y, 10, 40)
+    love.graphics.rectangle('fill', 380, opponent.y, 10, 40)
   end
 end
